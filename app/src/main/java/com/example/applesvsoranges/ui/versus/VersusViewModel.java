22b53c09911dd490c42dddff82759ca494850bc2
Ahmed Khattab <akhattab451@gmail.com>
2021-05-16 10:41:09 +0200

    Added Exception Handling for api response

--- app/src/main/java/com/example/applesvsoranges/ui/versus/VersusViewModel.java
+++ app/src/main/java/com/example/applesvsoranges/ui/versus/VersusViewModel.java
@@ -1,12 +1,9 @@
 package com.example.applesvsoranges.ui.versus;
 
-import android.os.Bundle;
 
 import com.example.applesvsoranges.domain.Fruit;
 import com.example.applesvsoranges.repository.DictionaryRepository;
 import javax.inject.Inject;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 import dagger.hilt.android.lifecycle.HiltViewModel;
@@ -19,30 +16,17 @@ public class VersusViewModel extends ViewModel {
     private DictionaryRepository mDictionaryRepository;
     private SavedStateHandle mSavedStateHandle;
 
-    private CompositeDisposable disposables = new CompositeDisposable();
-
-
     @Inject
     public VersusViewModel(
             SavedStateHandle savedStateHandle,
             DictionaryRepository dictionaryRepository
     ) {
         mDictionaryRepository = dictionaryRepository;
-        mSavedStateHandle = savedStateHandle;
     }
 
-    public Flowable<Fruit> getAppleFlowable() {
-        return mDictionaryRepository.getFruit(mSavedStateHandle.get("apple"));
+    public Flowable<Fruit> getFruitFlowable(String query) {
+        return mDictionaryRepository.getFruit(query);
     }
 
-    public Flowable<Fruit> getOrangeFlowable() {
-        return mDictionaryRepository.getFruit(mSavedStateHandle.get("orange"));
-    }
-
-    @Override
-    protected void onCleared() {
-        super.onCleared();
-        disposables.clear();
-    }
 
 }
